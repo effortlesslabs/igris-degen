9a0ed1bacc4d6fc5fb0bc04da1ddf2564f7bd1a7
Nasim Akhtar <fnlog10@gmail.com>
2025-10-02 07:31:19 +0800

    updated demo url

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import { HIcon } from '@/components/ui/icon'
+import { Button } from '../ui/button'
 
 export function Footer() {
   return (
     <footer className="sticky flex justify-center items-center bottom-0 left-0 right-0 z-50 bg-background/80 backdrop-blur-sm border-t border-border">
-      <div className="container flex items-center justify-between px-6 py-4">
+      <div className="container flex items-center justify-center px-6 py-4 relative">
         {/* Left side - Icons */}
-        <div className="flex items-center space-x-4">
+        <div className="absolute left-6 flex items-center space-x-4">
           <a
             href="https://igris.bot"
             target="_blank"
@@ -18,8 +19,19 @@ export function Footer() {
           </a>
         </div>
 
+        <Button
+          size="sm"
+          className="bg-primary/40 hover:bg-primary/60"
+          onClick={() => {
+            window.open('https://screen.studio/share/qXT84mzW', '_blank')
+          }}
+        >
+          <HIcon icon="mdi:play-circle" iconClassName="size-5" />
+          Watch Demo
+        </Button>
+
         {/* Right side - Text */}
-        <div className="text-base font-medium text-muted-foreground">
+        <div className="absolute right-6 text-base font-medium text-muted-foreground">
           try chat based{' '}
           <a
             href="https://beta.igris.bot"
